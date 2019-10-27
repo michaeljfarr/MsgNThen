d42961beef20b642c5fc7dbdc99878f6dbf6cf06
Michael Farr <michaeljfarr@hotmail.com>
2019-10-27 18:36:35 +1100

    Fix MsgNThenApdapterTests

--- MsgNThen.Adapter.Tests/MsgNThenApdapterTests.cs
+++ MsgNThen.Adapter.Tests/MsgNThenApdapterTests.cs
@@ -6,6 +6,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MsgNThen.Interfaces;
@@ -18,27 +19,37 @@ using Xunit;
 namespace MsgNThen.Adapter.Tests
 {
     
-    public class MsgnThenBuilderTests
+    public class MsgNThenApdapterTests
     {
-        public class TestContext
+        public class DummyType
         {
 
         }
 
         [Fact]
-        public async Task TestAppBuilder()
+        public async Task TestMessageProperties()
         {
             var serviceProvider = CreateServiceProvider();
-            var messageGroupHandler = serviceProvider.GetRequiredService<IMessageGroupHandler>();
             var apdapterFactory = serviceProvider.GetRequiredService<IMsgNThenApdapterFactory>();
-            var logger = serviceProvider.GetRequiredService<ILogger<MsgnThenBuilderTests>>();
-            var httpApplication = Substitute.For<IHttpApplication<TestContext>>();
+            var httpApplication = Substitute.For<IHttpApplication<DummyType>>();
+            
             var cancellationTokenSource = new CancellationTokenSource();
             var adapter = apdapterFactory.Start(httpApplication, cancellationTokenSource.Token);
+            var body = new byte[0];
             IHandledMessage message = new HandledMessageWrapper(new BasicDeliverEventArgs(
-                "consumer", 1, false, "ex", "rout", new BasicProperties(), new byte[0]));
+                "consumer", 1, false, "ex", "rout", new BasicProperties(){MessageId = Guid.NewGuid().ToString()}, body));
             var res = await adapter.HandleMessageTask(message);
             res.Should().Be(AckResult.Ack);
+            httpApplication.Received().CreateContext(Arg.Is<IFeatureCollection>(x =>
+                CheckRequest(x, message, body)
+            ));
+        }
+
+        private static bool CheckRequest(IFeatureCollection x, IHandledMessage message, byte[] body)
+        {
+            x.Get<IHttpRequestFeature>().Headers["MessageId"].ToString().Should().Be( message.Properties.MessageId);
+            ((MemoryStream)x.Get<IHttpRequestFeature>().Body).ToArray().Should().BeEquivalentTo(body);
+            return true;
         }
 
         public static ServiceProvider CreateServiceProvider()
